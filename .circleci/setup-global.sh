c6abe2abde002e26acc9984e1d9bc6c703c98536
Matt <tam.nguyen@ffwagency.com>
2017-09-14 14:05:31 +0700

    Install heroku

--- .circleci/setup-global.sh
+++ .circleci/setup-global.sh
@@ -1,10 +1,7 @@
-apt-get install software-properties-common
+sudo apt-get install software-properties-common
 sudo add-apt-repository "deb https://cli-assets.heroku.com/branches/stable/apt ./"
-
 curl -L https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
-
 sudo apt-get update
-
 sudo apt-get install heroku
 
 cat > ~/.netrc << EOF
