1442e198fb105beaf5ab7f4a6d23a09d640fe3fd
Matt <tam.nguyen@ffwagency.com>
2017-09-14 14:24:42 +0700

    Update script build

--- .circleci/build-styguide.sh
+++ .circleci/build-styguide.sh
@@ -4,22 +4,7 @@ BUILD_DIR=$(pwd)
 JSON=composer.json
 EXE=composer
 
-# CI options
-export COMPOSER_DISCARD_CHANGES=1
-export COMPOSER_NO_INTERACTION=1
-
-if [ ! -f "$JSON" ]
-then
-  echo ${txtred}Error: No $JSON found ${txtrst}
-  exit 1
-fi
-
 FOUND=`which $EXE`
-COMPOSER_PARMS="--no-ansi --no-dev --no-interaction --optimize-autoloader --no-progress"
-
-# Install Composer dependencies
-echo -e "\n${txtylw}Invoking: $FOUND install $COMPOSER_PARMS ${txtrst}"
-$FOUND install $COMPOSER_PARMS
 
 # generate styleguide from pattern lab
 
