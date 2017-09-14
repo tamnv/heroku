fa0d8b836f09e6f7a95681ce917358b0ad942e49
Matt <tam.nguyen@ffwagency.com>
2017-09-14 14:07:52 +0700

    install apt-transport-https

--- .circleci/setup-global.sh
+++ .circleci/setup-global.sh
@@ -1,6 +1,7 @@
 sudo apt-get install software-properties-common
 sudo add-apt-repository "deb https://cli-assets.heroku.com/branches/stable/apt ./"
 curl -L https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
+sudo apt-get install apt-transport-https
 sudo apt-get update
 sudo apt-get install heroku
 
